11b6ec9109d75024c8d9f56b13fd8b40d45aca2b
Gus Narea <gnarea@users.noreply.github.com>
2021-10-29 12:29:11 +0100

    fix defaultEndpoints

--- src/defaultEndpoints.ts
+++ src/defaultEndpoints.ts
@@ -9,7 +9,7 @@ const DEFAULT_PUBLIC_ENDPOINT = 'ping.awala.services';
 const IS_TYPESCRIPT = __filename.endsWith('.ts');
 /* istanbul ignore next */
 const ROOT_DIR = IS_TYPESCRIPT ? dirname(__dirname) : dirname(dirname(__dirname));
-const DEFAULT_PUBLIC_ENDPOINT_CERT_PATH = join(ROOT_DIR, 'data', 'ping-awala-services-id-cert.der');
+const DEFAULT_CONNECTION_PARAMS_PATH = join(ROOT_DIR, 'data', 'default-connection-params.der');
 
 export async function getDefaultThirdPartyEndpoint(): Promise<ThirdPartyEndpoint> {
   const existingEndpoint = await PublicThirdPartyEndpoint.load(DEFAULT_PUBLIC_ENDPOINT);
@@ -17,8 +17,8 @@ export async function getDefaultThirdPartyEndpoint(): Promise<ThirdPartyEndpoint
     return existingEndpoint;
   }
 
-  const idCertificate = await fs.readFile(DEFAULT_PUBLIC_ENDPOINT_CERT_PATH);
-  return PublicThirdPartyEndpoint.import(DEFAULT_PUBLIC_ENDPOINT, idCertificate);
+  const connectionParamsFile = await fs.readFile(DEFAULT_CONNECTION_PARAMS_PATH);
+  return PublicThirdPartyEndpoint.import(connectionParamsFile);
 }
 
 export async function getDefaultFirstPartyEndpoint(): Promise<FirstPartyEndpoint> {
