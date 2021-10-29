8952d1363ad94f0a6e836783424700560b365a24
Gus Narea <gnarea@users.noreply.github.com>
2021-10-29 17:33:20 +0100

    fix

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,10 +1,10 @@
 import { GSCClient } from '@relaycorp/relaynet-core';
 import { Paths } from 'env-paths';
-import { Logger } from 'pino';
+import { P as pino } from 'pino';
 import { Token } from 'typedi';
 
 export const APP_DIRS = new Token<Paths>('APP_DIRS');
 
-export const LOGGER = new Token<Logger>('LOGGER');
+export const LOGGER = new Token<pino.Logger>('LOGGER');
 
 export const GSC_CLIENT = new Token<GSCClient>('GSC_CLIENT');
