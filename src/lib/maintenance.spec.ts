485a88356bcc84a92228442d337c1c018685063c
Gus Narea <gnarea@users.noreply.github.com>
2022-03-31 22:59:29 +0100

    stub tests

---
+++ src/lib/maintenance.spec.ts
@@ -0,0 +1,7 @@
+describe('runMaintenance', () => {
+  describe('Expiring certificates', () => {
+    test.todo('Certificates with fewer than 90 days left should be renewed');
+
+    test.todo('Certificates with more than 90 days left should not be renewed');
+  });
+});
---
+++ src/lib/maintenance.ts
@@ -0,0 +1,3 @@
+export async function runMaintenance(): Promise<void> {
+  throw new Error();
+}
